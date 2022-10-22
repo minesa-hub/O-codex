7a0234b7bce35a7eee747b8c49fd42aef59009d0
Neo <neoaichan@gmail.com>
2022-10-22 18:38:17 +0300

    Add files via upload

---
+++ src/event/interactionCreate.js
@@ -0,0 +1,19 @@
+import { InteractionType } from "discord.js";
+import { readdirSync } from "fs";
+
+export default {
+    name: "interactionCreate",
+    execute: async interaction => {
+        let client = interaction.client;
+        if (interaction.type == InteractionType.ApplicationCommand) {
+            if (interaction.user.bot) return;
+
+            readdirSync("./src/commands").forEach(async file => {
+                const command = await import(`../../src/commands/${file}`).then(x => x.default);
+                if (interaction.commandName.toLowerCase() === command.data.name.toLowerCase()) {
+                    command.run(client, interaction);
+                }
+            });
+        }
+    },
+};
---
+++ src/event/messageCreate.js
@@ -0,0 +1,10 @@
+import config from "../config.js";
+
+export default {
+    name: "messageCreate",
+    execute: async message => {
+        let client = message.client;
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+    },
+};
