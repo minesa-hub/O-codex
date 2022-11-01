fe570d690e166102ab8624ac8d4a658ffd2075a1
Neodevils <neoaichan@gmail.com>
2022-11-02 01:13:15 +0300

    Removed .setActivity()

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,6 @@
 export const name = "ready";
 export const once = true;
 export async function execute(client) {
-    await client.application.fetch();
-    client.user.setActivity("/help", { type: "LISTENING" });
-    console.log(`Bot is now online! Logged in as ${client.user.tag}.`);
     // Fetching the message on rules channel to bot does not die from restart.
     client.channels.cache.get("889266702620176434").messages.fetch("1030252706696269854");
 }
