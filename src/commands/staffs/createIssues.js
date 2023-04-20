582aa2c606829f9652065998c8f4190c402d791a
Neo <71941576+Neodevils@users.noreply.github.com>
2023-04-20 19:57:19 +0300

    Updates on Commands and Renaming (#33)

---
+++ src/commands/staffs/createIssues.js
@@ -0,0 +1,126 @@
+import {
+    SlashCommandBuilder,
+    ButtonBuilder,
+    ActionRowBuilder,
+    EmbedBuilder,
+    ButtonStyle,
+    ChannelType,
+    PermissionFlagsBits,
+} from "discord.js";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("setup-ticket")
+        .setDescription("Setup ticket system with threads.")
+        .addStringOption((option) =>
+            option
+                .setName("title")
+                .setDescription("Set title of embed message")
+                .setRequired(false),
+        )
+        .addStringOption((option) =>
+            option
+                .setName("description")
+                .setDescription("Set description of embed message")
+                .setRequired(false),
+        )
+        .addStringOption((option) =>
+            option
+                .setName("color")
+                .setDescription("Set color of embed message")
+                .setRequired(false)
+                .addChoices(
+                    { name: "Lilac", value: "#D9B2FF" },
+                    { name: "Powder Blue", value: "#BFEFFF" },
+                    { name: "Mauve", value: "#FFB6C1" },
+                    { name: "Pale Green", value: "#C8FFB0" },
+                    { name: "Ivory", value: "#FFFFF0" },
+                    { name: "Slate", value: "#B0C4DE" },
+                    { name: "Mint", value: "#BDFCC9" },
+                    { name: "Lavender Gray", value: "#C4C3D0" },
+                    { name: "Pink", value: "#FFC0CB" },
+                    { name: "Silver", value: "#C0C0C0" },
+                    { name: "Peach", value: "#FFE5B4" },
+                    { name: "Pale Yellow", value: "#FFFFB2" },
+                    { name: "Light Gray", value: "#D3D3D3" },
+                    { name: "Lavender", value: "#E6E6FA" },
+                    { name: "Sky Blue", value: "#87CEEB" },
+                    { name: "Beige", value: "#F5F5DC" },
+                    { name: "Salmon", value: "#FFA07A" },
+                    { name: "Platinum", value: "#E5E4E2" },
+                    { name: "Misty Rose", value: "#FFE4E1" },
+                    { name: "Light Cyan", value: "#E0FFFF" },
+                    { name: "Light Pink", value: "#FFB6C1" },
+                    { name: "Pale Turquoise", value: "#AFEEEE" },
+                    { name: "Light Salmon", value: "#FFA07A" },
+                    { name: "Black", value: "#000000" },
+                    { name: "White", value: "#FFFFFF" },
+                ),
+        ),
+    async execute({ interaction, client }) {
+        if (
+            !interaction.member.permissions.has(
+                PermissionFlagsBits.ManageThreads ||
+                    PermissionFlagsBits.ManageChannels ||
+                    PermissionFlagsBits.Administrator,
+            )
+        )
+            return interaction.reply({
+                content:
+                    "You **can not** setup this system. You need `Manage Threads`, `Manage Channels` or `Administrator` permission to setup this system.",
+                ephemeral: true,
+            });
+
+        if (interaction.channel.type !== ChannelType.GuildText)
+            return interaction.reply({
+                content:
+                    "You **can not** setup this system in this channel. Please try again in __Text Channel__ type channel.",
+                ephemeral: true,
+            });
+
+        const embedTitle = interaction.options.getString("title");
+        const embedDescription = interaction.options.getString("description");
+        const embedColor = interaction.options.getString("color");
+
+        const createIssueEmoji = client.emojis.cache.find(
+            (emoji) => emoji.name === "issue_open_button",
+        );
+
+        const embed = new EmbedBuilder()
+            .setTitle(
+                embedTitle ? embedTitle : "Welcome to our support portal!",
+            )
+            .setDescription(
+                embedDescription
+                    ? embedDescription
+                    : `If you're experiencing an issue with our product or service, please use the "Issue Creation" button to report it. This includes any server-related issues you may be encountering in our Discord server.
+
+When creating a new issue, please provide a clear summary of the problem and any steps you took before encountering it. This information will help us resolve the issue as quickly as possible.
+
+Thank you for helping us improve our product/service and Discord server!`,
+            )
+            .setColor(embedColor ? embedColor : "Random")
+            .setThumbnail(
+                "https://media.discordapp.net/attachments/861208192121569280/1097190943528067122/C540848F-771D-4676-835C-6F65B6A54FF4.png",
+            )
+            .setFooter({
+                text: interaction.guild.name,
+                iconURL: interaction.guild.iconURL(),
+            });
+
+        const createIssueButton = new ButtonBuilder()
+            .setCustomId("create-issue")
+            .setLabel("Create Issue")
+            .setStyle(ButtonStyle.Success)
+            .setEmoji(`${createIssueEmoji}`);
+
+        const row = new ActionRowBuilder().addComponents(createIssueButton);
+        
+        interaction.reply({
+            content: "Created the system succesfully!",
+            ephemeral: true,
+        });
+
+        interaction.channel.send({ embeds: [embed], components: [row] });
+    },
+};
---
+++ src/components/modals/create-issue-title.js
@@ -0,0 +1,78 @@
+import {
+    ChannelType,
+    ActionRowBuilder,
+    EmbedBuilder,
+    StringSelectMenuBuilder,
+    StringSelectMenuOptionBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+} from "discord.js";
+
+let lockButton;
+
+export default {
+    data: {
+        customId: "create-issue-modal",
+    },
+    execute: async ({ interaction }) => {
+        const issueTitle = interaction.fields.getTextInputValue("issue-title");
+        const embed = new EmbedBuilder()
+            .setTitle("Now, we are here…")
+            .setDescription(
+                "Our staff member(s) will take care of this issue sooner. While they are on their way, why don’t you talk about your issue?",
+            )
+            .setColor("NotQuiteBlack")
+            .setThumbnail(
+                "https://media.discordapp.net/attachments/861208192121569280/1097266882521878608/B7609F62-85F1-4C84-B58F-FE66AFBB1519.png",
+            );
+
+        const menu = new StringSelectMenuBuilder()
+            .setCustomId("issue-select-menu")
+            .setDisabled(false)
+            .setMaxValues(1)
+            .setPlaceholder("Action to close issue")
+            .addOptions(
+                new StringSelectMenuOptionBuilder()
+                    .setLabel("Close as completed")
+                    .setValue("issue-menu-close")
+                    .setDescription("Done, closed, fixed, resolved")
+                    .setEmoji("<:issue_closed:1097273507383103631>")
+                    .setDefault(false),
+                new StringSelectMenuOptionBuilder()
+                    .setLabel("Close as not planned")
+                    .setValue("issue-menu-duplicate")
+                    .setDescription("Won’t fix, can’t repo, duplicate, stale")
+                    .setEmoji("<:skip:1097273560738832437>"),
+            );
+
+        const menuRow = new ActionRowBuilder().addComponents(menu);
+        lockButton = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId("issue-lock-conversation")
+                .setLabel("Lock Issue")
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(false)
+                .setEmoji("<:lock_button:1097581058876256347>"),
+        );
+
+        let thread = await interaction.channel.threads.create({
+            name: `${issueTitle}`,
+            autoArchiveDuration: 60,
+            type: ChannelType.PrivateThread,
+            reason: `${interaction.user.username} opened an issue`,
+            invitable: false,
+        });
+        await interaction.reply({
+            content: `Created an issue: <#${thread.id}>`,
+            ephemeral: true,
+        });
+        let pinMessage = await thread.send({
+            embeds: [embed],
+            components: [menuRow, lockButton],
+        });
+        await pinMessage.pin();
+        await thread.members.add(interaction.user);
+    },
+};
+
+export { lockButton };
