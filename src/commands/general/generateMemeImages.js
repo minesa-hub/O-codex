b9fb6806410a0159549cd2050c2d69dc7fb000f8
İbrahim <71941576+Neodevils@users.noreply.github.com>
2023-04-24 21:32:12 +0300

    Added comment on meme generating images

--- src/commands/general/generateMemeImages.js
+++ src/commands/general/generateMemeImages.js
@@ -1,8 +1,11 @@
+// Importing the required modules
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { fetch } from "undici";
 import { alertEmoji } from "../../shortcuts/emojis.js";
 
+// Exporting the command
 export default {
+    // The command data
     data: new SlashCommandBuilder()
         .setName("send")
         .setNameLocalizations({
@@ -32,23 +35,32 @@ export default {
                 }),
         )
         .setDMPermission(false),
+    // The command output
     execute: async ({ interaction }) => {
+        // Deferring the reply
         await interaction.deferReply();
 
+        // Trying to fetch the meme
         try {
+            // Fetching the meme from the API
             const raw = await fetch("https://apis.duncte123.me/meme", {
                 method: "GET",
             });
+            // Parsing the response
             const response = await raw.json();
-
+            // Creating the embed
             const embed = new EmbedBuilder()
                 .setTitle(response.data.title)
                 .setImage(response.data.image)
                 .setColor(0x1e1e1e);
 
+            // Editing the deferred reply
             return interaction.editReply({ embeds: [embed] });
+            // Catching the error
         } catch (error) {
+            // Logging the error
             console.error(error);
+            // Editing the deferred reply
             return interaction.editReply({
                 content: `${alertEmoji} An error occurred while trying to fetch the meme. Try again in some time later.`,
                 ephemeral: true,
