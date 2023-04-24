467d1fc7fd5e2b4e90ff618274408f46ebdd2f3e
İbrahim <71941576+Neodevils@users.noreply.github.com>
2023-04-24 21:26:42 +0300

    Added comment on user avatar

--- src/commands/contextMenu/userAvatar.js
+++ src/commands/contextMenu/userAvatar.js
@@ -1,10 +1,17 @@
+/* 
+    Description: This command will send the user's avatar when the user right-clicks on their name in the server.
+ */
+
+// Importing the required modules
 import {
     ContextMenuCommandBuilder,
     ApplicationCommandType,
     EmbedBuilder,
 } from "discord.js";
 
+// Exporting the command
 export default {
+    // The command data
     data: new ContextMenuCommandBuilder()
         .setName("User Avatar")
         .setNameLocalizations({
@@ -14,22 +21,29 @@ export default {
         })
         .setType(ApplicationCommandType.User)
         .setDMPermission(false),
+    // The command output
     execute: async ({ interaction }) => {
+        // Deferring the reply
         await interaction.deferReply({ ephemeral: true });
 
+        // Getting the target member from the interaction target ID
         const target = interaction.guild.members.cache.get(
             interaction.targetId,
         );
+
+        // Getting the avatar URL of the target member
         const avatar = target.user.displayAvatarURL({
             dynamic: true,
             size: 4096,
         });
 
+        // Creating the embed
         const embed = new EmbedBuilder()
             .setTitle(`${target.user.tag}'s Avatar`)
             .setImage(avatar)
             .setColor(0x1e1e1e);
 
+        // Editing the deferred reply
         await interaction.editReply({
             embeds: [embed],
         });
