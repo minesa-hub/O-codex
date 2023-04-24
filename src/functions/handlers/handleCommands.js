1d4a3aaa6d8dc173e5118bac3a9bf10c2d2d3d21
İbrahim <71941576+Neodevils@users.noreply.github.com>
2023-04-24 22:23:37 +0300

    Added comment on Handlers

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,32 +1,47 @@
 // const commandFolders = `${__dirname}/../../commands`; for panels
+
+// Importing the modules
 import { REST, Routes } from "discord.js";
 import { promises as fs } from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import chalk from "chalk";
 
+// Getting the current file path
 const __filename = fileURLToPath(import.meta.url);
+// Getting the current directory path
 const __dirname = dirname(__filename);
 
+// Exporting the function
 export default async (client) => {
+    // Defining the handleCommands function
     client.handleCommands = async () => {
+        // Getting all the folders in the commands folder
         const commandFolders = await fs.readdir(
             `${__dirname}../../../commands`,
         );
 
+        // Looping through all the folders
         for (const folder of commandFolders) {
+            // Getting all the files in the folder
             const commandFiles = (
                 await fs.readdir(`${__dirname}../../../commands/${folder}`)
             ).filter((file) => file.endsWith(".js"));
-
+            // Defining the commands and commandArray variables
             const { commands, commandArray } = client;
+
+            // Looping through all the files
             for (const file of commandFiles) {
+                // Importing the command
                 const { default: command } = await import(
                     `../../commands/${folder}/${file}`
                 );
+                // Setting the command in the commands collection
                 commands.set(command.data.name, command);
+                // Pushing the command data to the commandArray
                 commandArray.push(command.data.toJSON());
 
+                // Logging the command name in green color
                 console.log(
                     chalk.green(
                         `[Commands]: Loaded ${command.data.name} command.`,
@@ -34,27 +49,35 @@ export default async (client) => {
                 );
             }
         }
-
+        // Defining the client id
         const clientId = "736561919292473454";
-        // const guildId = '795473336998035486';
+        /* Defining the guild id
+        const guildId = '795473336998035486'; 
+        */
+        // Defining the rest variable and setting the token and version
         const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
         try {
+            // Logging the refreshing application commands
             console.log(
                 chalk.cyan(
                     `[Commands] Started refreshing application (/) commands.`,
                 ),
             );
+
+            // Putting the application commands
             await rest.put(Routes.applicationCommands(clientId), {
                 body: client.commandArray,
             });
 
+            // Logging the refreshing application commands
             console.log(
                 chalk.cyan(
                     `[Commands] Successfully reloaded application (/) commands.`,
                 ),
             );
         } catch (error) {
+            // Logging the error
             console.error(error);
         }
     };
--- src/functions/handlers/handleEvents.js
+++ src/functions/handlers/handleEvents.js
@@ -1,37 +1,56 @@
+// Import the fs package and the chalk package
 import fs from "fs";
 import chalk from "chalk";
 
+// Importing the mongoose package and destructuring the connection property
 import mongoose from "mongoose";
 const { connection } = mongoose;
 
+// Exporting the function
 export default async (client) => {
+    // Defining the handleEvents function
     client.handleEvents = async () => {
+        // Getting all the folders in the events folder
         const eventFolders = fs.readdirSync("./src/events");
 
+        // Looping through all the folders
         for (const folder of eventFolders) {
+            // Getting all the files in the folder
             const eventFiles = fs
                 .readdirSync(`./src/events/${folder}`)
                 .filter((file) => file.endsWith(".js"));
 
+            // Switching through the folder name
             switch (folder) {
+                // If the folder name is client
                 case "client":
+                    // Looping through all the files
                     for (const file of eventFiles) {
+                        // Importing the event
                         const { default: event } = await import(
                             `../../events/${folder}/${file}`
                         );
+
+                        // Checking if the event is once
                         if (event.once) {
+                            // If the event is once, then add the event to the client once collection
                             client.once(event.name, (...args) =>
                                 event.execute(...args, client),
                             );
+
+                            // Logging the event name in green color
                             console.log(
                                 chalk.green(
                                     `[Events]: Loaded ${event.name} event.`,
                                 ),
                             );
                         } else {
+                            // If the event is not once, then add the event to the client on collection
                             client.on(event.name, (...args) =>
                                 event.execute(...args, client),
                             );
+
+                            // Logging the event name in green color
                             console.log(
                                 chalk.green(
                                     `[Events]: Loaded ${event.name} event.`,
@@ -41,24 +60,35 @@ export default async (client) => {
                     }
                     break;
 
+                // If the folder name is mongo
                 case "mongo":
+                    // Looping through all the files
                     for (const file of eventFiles) {
+                        // Importing the event
                         const { default: event } = await import(
                             `../../events/${folder}/${file}`
                         );
+
+                        // Checking if the event is once
                         if (connection.once) {
+                            // If the event is once, then add the event to the connection once collection
                             connection.once(event.name, (...args) =>
                                 event.execute(...args, client),
                             );
+
+                            // Logging the event name in green color
                             console.log(
                                 chalk.green(
                                     `[Events]: Loaded ${event.name} event.`,
                                 ),
                             );
                         } else {
+                            // If the event is not once, then add the event to the connection on collection
                             connection.on(event.name, (...args) =>
                                 event.execute(...args, client),
                             );
+
+                            // Logging the event name in green color
                             console.log(
                                 chalk.green(
                                     `[Events]: Loaded ${event.name} event.`,
@@ -67,7 +97,9 @@ export default async (client) => {
                         }
                     }
                     break;
+                // If the folder name is not client or mongo
                 default:
+                    // do nothing again :D
                     break;
             }
         }
