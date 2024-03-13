a26c78bfe6ef95685fea7e43528143831e54f93b
Neodevil <neoibrahim.gunes@gmail.com>
2024-03-14 02:41:06 +0300

    Added new permission checker

---
+++ src/shortcuts/permissionErrors.js
@@ -0,0 +1,55 @@
+import { PermissionFlagsBits, bold } from "discord.js";
+import { exclamationmark_triangleEmoji } from "./emojis.js";
+
+// For bot
+export const defaultPermissionErrorForBot = (
+    interaction,
+    permission,
+    additionalText = ""
+) => {
+    const PERMISSION_NAME = Object.keys(PermissionFlagsBits).find(
+        (key) => PermissionFlagsBits[key] === permission
+    );
+    const checkingPermission =
+        interaction.guild.members.me.permissions.has(permission);
+
+    if (!checkingPermission) {
+        interaction.reply({
+            content: `${exclamationmark_triangleEmoji} I don't have ${bold(
+                PERMISSION_NAME
+            )} permission to do this action, <@${interaction.user.id}>. ${
+                additionalText == "" ? "" : "\n> " + additionalText
+            }`,
+            ephemeral: true,
+        });
+        return true;
+    }
+
+    return false;
+};
+
+// For user
+export const defaultPermissionErrorForMember = async (
+    interaction,
+    permission,
+    additionalText = ""
+) => {
+    const PERMISSION_NAME = Object.keys(PermissionFlagsBits).find(
+        (key) => PermissionFlagsBits[key] === permission
+    );
+    const checkingPermission = interaction.member.permissions.has(permission);
+
+    if (!checkingPermission) {
+        interaction.reply({
+            content: `${exclamationmark_triangleEmoji} You don't have ${bold(
+                PERMISSION_NAME
+            )} permission to do this action, <@${interaction.user.id}>. ${
+                additionalText == "" ? "" : "\n> " + additionalText
+            }`,
+            ephemeral: true,
+        });
+        return true;
+    }
+
+    return false;
+};
